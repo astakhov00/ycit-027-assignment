02ccb5b962d65856e7bff1db9f9f52f9500688df
= <astakhov00@gmail.com>
2022-12-04 17:02:21 -0500

    Assignment completed

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 function getCatImage() {
-    fetch("https://api.thecatapi.com/v1/images/search")
-        .then((response) => {
-            // Resolve the promise we got from calling then() to the promise that we get from calling json()
-            response.json();
-        })
-        .then((data) => {
-            console.log(data[0].url);
-            document.getElementById("catImage").src = data[0].url;
-        });
+  const addImgElem = document.createElement("img");
+  const imgContainer = document.getElementById("imgContainer");
+  fetch("https://api.thecatapi.com/v1/images/search")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data[0].url);
+      imgContainer.appendChild(addImgElem).src = data[0].url;
+    });
 }
